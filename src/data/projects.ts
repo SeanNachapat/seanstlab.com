e4b3352c4534c686ba0840caac01851ce84436a7
Nachapat Iamphuang <61649121+SeanNachapat@users.noreply.github.com>
2025-12-09 10:12:33 +0700

    🏗️ Update Image path

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -11,26 +11,26 @@ export interface Project {
 export const projects: Project[] = [
   {
     id: 1,
-    title:"Go-Shrimp",
-    description:"Web Application for Shrimp Farmers integrated with MongoDB database, image classification and disease prediction",
-    imageUrl:"",
-    pageLink:"https://github.com/SeanNachapat/go-shrimp",
-    tags:["Javascript", "NEXT.js", "Vercel", "MongoDB", "GeminiAPI"]
+    title: "Go-Shrimp",
+    description: "Web Application for Shrimp Farmers integrated with MongoDB database, image classification and disease prediction",
+    imageUrl: "",
+    pageLink: "https://github.com/SeanNachapat/go-shrimp",
+    tags: ["Javascript", "NEXT.js", "Vercel", "MongoDB", "GeminiAPI"]
   },
   {
     id: 3,
-    title:"N-Pochna | Diet Assistant",
-    description:"This project is made for educational purposes as a senior project at Kasetsart University Laboratory School Multilingual Program. N'Pochna is a LINE Bot integrated with Edamam API to assist in managing diets and workouts. Made by Nachapat Iamphuang.",
-    imageUrl:"public/Project/N-Pochna.png",
-    pageLink:"https://github.com/SeanNachapat/N-Pochna",
-    tags:["Javascript", "Node.js", "LINE", "RapidAPI", "Ngrok"]
+    title: "N-Pochna | Diet Assistant",
+    description: "This project is made for educational purposes as a senior project at Kasetsart University Laboratory School Multilingual Program. N'Pochna is a LINE Bot integrated with Edamam API to assist in managing diets and workouts. Made by Nachapat Iamphuang.",
+    imageUrl: "/images/N-Pochna.png",
+    pageLink: "https://github.com/SeanNachapat/N-Pochna",
+    tags: ["Javascript", "Node.js", "LINE", "RapidAPI", "Ngrok"]
   },
   {
     id: 2,
-    title:"What's The Word",
-    description:"This project was created for educational purposes by Computer Science students at KMITL. What's The Word? is a web application game for Thai students to practice their knowledge of English vocabulary, categorized by the Common European Framework of Reference for Languages(CEFR).",
-    imageUrl:"public/Project/wtw.png",
-    pageLink:"https://github.com/SeanNachapat/whatstheword",
-    tags:["Javascript", "NEXT.js", "Vercel"]
+    title: "What's The Word",
+    description: "This project was created for educational purposes by Computer Science students at KMITL. What's The Word? is a web application game for Thai students to practice their knowledge of English vocabulary, categorized by the Common European Framework of Reference for Languages(CEFR).",
+    imageUrl: "/images/wtw.png",
+    pageLink: "https://github.com/SeanNachapat/whatstheword",
+    tags: ["Javascript", "NEXT.js", "Vercel"]
   }
 ];

ea1632b15b726a8ce07b638c99ae5316d51d534c
Nachapat Iamphuang <SeanNachapat@gmail.com>
2026-01-06 22:38:45 +0700

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -13,7 +13,7 @@ export const projects: Project[] = [
     id: 1,
     title: "Go-Shrimp",
     description: "Web Application for Shrimp Farmers integrated with MongoDB database, image classification and disease prediction",
-    imageUrl: "/images/go-shrimp.png",
+    imageUrl: "/images/GoShrimp.png",
     pageLink: "https://github.com/SeanNachapat/go-shrimp",
     tags: ["Javascript", "NEXT.js", "Vercel", "MongoDB", "GeminiAPI"]
   },
